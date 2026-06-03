9b5d8c7e13cf818c8860ed0a8d77914fce97e167
Htet Aung <25aw0126@jec.ac.jp>
2026-06-03 14:05:22 +0900

    Complete Firebase CRUD and environment setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ import { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { register, login, logout } from "./auth";
 import { auth } from "./firebase";
+import { getUsers,updateUserRole,deleteUser } from "./firestore";
 
 function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const [currentUser, setCurrentUser] = useState(null);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   const handleRegister = async () => {
     try {
@@ -26,13 +38,33 @@ function App() {
   };
 
   const handleLogout = async () => {
-    await logout();
-    alert("Logout success");
+    try {
+      await logout();
+      setUsers([]);
+      alert("Logout success");
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const loadUsers = async () => {
+    try {
+      const data = await getUsers();
+      setUsers(data);
+    } catch (error) {
+      alert(error.message);
+    }
   };
 
   return (
     <div>
       <h1>Firebase Login Test</h1>
+
+      {currentUser ? (
+        <p>Login User: {currentUser.email}</p>
+      ) : (
+        <p>No user logged in</p>
+      )}
 
       <input
         type="email"
@@ -55,6 +87,34 @@ function App() {
       <button onClick={handleRegister}>Register</button>
       <button onClick={handleLogin}>Login</button>
       <button onClick={handleLogout}>Logout</button>
+
+      <hr />
+
+      <button onClick={loadUsers}>Load Users</button>
+
+      <h2>Users</h2>
+
+      {users.map((user) => (
+  <div key={user.id}>
+    <p>{user.email}</p>
+
+    <button
+      onClick={() =>
+        updateUserRole(user.id)
+      }
+    >
+      Make Admin
+    </button>
+    <button
+   onClick={async () => {
+    await deleteUser(user.id);
+    await loadUsers();
+  }}
+>
+  Delete User
+</button>
+  </div>
+))}
     </div>
   );
 }
